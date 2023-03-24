406d42b1d418279bbc598923487786156adfc858
CarlosMol <caliche187@hotmail.com>
2023-03-24 17:02:58 -0500

    test

---
+++ GetEntregasInfo/index.js
@@ -0,0 +1,19 @@
+module.exports = async function (context, req) {
+  context.log("Función GetTostadas activada.");
+
+  // Lista de marcas de tostadas
+  const tostadas = [
+    { id: 1, marca: "Tosty" },
+    { id: 2, marca: "Bimbo" },
+    { id: 3, marca: "Holsum" },
+  ];
+
+  // Devuelve la lista de marcas de tostadas en la respuesta
+  context.res = {
+    status: 200,
+    body: tostadas,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
