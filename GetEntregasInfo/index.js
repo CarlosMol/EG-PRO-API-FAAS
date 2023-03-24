ea6067fc94fc9790f01717ec6ddf48b7464ed49d
CarlosMol <caliche187@hotmail.com>
2023-03-24 17:38:32 -0500

    fix function

--- GetEntregasInfo/index.js
+++ GetEntregasInfo/index.js
@@ -1,19 +1,36 @@
+const cors = require("cors")();
+const { exec } = require("child_process");
+
 module.exports = async function (context, req) {
-  context.log("Función GetTostadas activada.");
+  cors(req, context.res, () => {
+    const destinations = req.body.destinations || "6.84,-75.46";
+    const origins = req.body.origins || "7.83,-75.46";
+    const mode = "driving";
+    const key = "AIzaSyAEXeXklXIIQ1L8RdL2i4YKFC5hNF2b9R4"; // Reemplaza esto con tu propia API key
+    const urlMaps = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destinations}&origins=${origins}&mode=${mode}&key=${key}`;
 
-  // Lista de marcas de tostadas
-  const tostadas = [
-    { id: 1, marca: "Tosty" },
-    { id: 2, marca: "Bimbo" },
-    { id: 3, marca: "Holsum2345" },
-  ];
-
-  // Devuelve la lista de marcas de tostadas en la respuesta
-  context.res = {
-    status: 200,
-    body: tostadas,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    exec(`curl ${urlMaps}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al ejecutar el comando: ${error}`);
+        context.res = {
+          status: 500,
+          body: "Error al hacer la solicitud a la API de Google Maps",
+          headers: {
+            "Content-Type": "text/plain",
+          },
+        };
+        context.done();
+        return;
+      }
+      const data = JSON.parse(stdout);
+      context.res = {
+        status: 200,
+        body: data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      context.done();
+    });
+  });
 };
