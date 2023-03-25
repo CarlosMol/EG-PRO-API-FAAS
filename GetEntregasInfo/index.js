a862b7855220548a2e264b51496d975266d1f395
CarlosMol <caliche187@hotmail.com>
2023-03-24 21:20:40 -0500

    fix 3 cors

--- GetEntregasInfo/index.js
+++ GetEntregasInfo/index.js
@@ -1,27 +1,33 @@
 const axios = require("axios");
-const cors = require('cors');
+const cors = require("cors");
 
 module.exports = async function (context, req) {
-    // Configuración de CORS
-    await cors(context.req, context.res);
+  // Configuración de CORS
+  await cors(context.req, context.res);
 
-    context.log("Consumiendo API externa con Azure Function...");
-    try {
-        const origins = req.query.origins;
-        const destinations = req.query.destinations;
-        const API_KEY = "AIzaSyAEXeXklXIIQ1L8RdL2i4YKFC5hNF2b9R4";
-        const url = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destinations}&origins=${origins}&mode=driving&key=${API_KEY}`
+  context.log("Consumiendo API externa con Azure Function...");
+  try {
+    const origins = req.query.origins;
+    const destinations = req.query.destinations;
+    const API_KEY = "AIzaSyAEXeXklXIIQ1L8RdL2i4YKFC5hNF2b9R4";
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destinations}&origins=${origins}&mode=driving&key=${API_KEY}`;
 
-        const response = await axios.get(url);
+    const response = await axios.get(url);
 
-        context.res = {
-            status: 200,
-            body: response.data
-        };
-    } catch (error) {
-        context.res = {
-            status: 500,
-            body: "Error al obtener información de la API externa."
-        };
-    }
+    context.res = {
+      status: 200,
+      body: response.data,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: "Error al obtener información de la API externa.",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  }
 };
