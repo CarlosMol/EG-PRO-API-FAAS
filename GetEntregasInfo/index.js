135890ba595c1342f69f9ee83693ba2d6b9df86f
CarlosMol <caliche187@hotmail.com>
2023-03-24 21:33:40 -0500

    rollback

--- GetEntregasInfo/index.js
+++ GetEntregasInfo/index.js
@@ -1,23 +1,23 @@
 const axios = require("axios");
-const express = require("azure-function-express").express;
-const cors = require("cors");
-const app = express();
 
-app.use(cors());
-
-app.get("/api/getEntregasInfo", async (req, res) => {
+module.exports = async function (context, req) {
+    context.log("Consumiendo API externa con Azure Function...");
     try {
         const origins = req.query.origins;
         const destinations = req.query.destinations;
         const API_KEY = "AIzaSyAEXeXklXIIQ1L8RdL2i4YKFC5hNF2b9R4";
-        const url = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destinations}&origins=${origins}&mode=driving&key=${API_KEY}`;
+        const url = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destinations}&origins=${origins}&mode=driving&key=${API_KEY}`
 
         const response = await axios.get(url);
 
-        res.status(200).json(response.data);
+        context.res = {
+            status: 200,
+            body: response.data
+        };
     } catch (error) {
-        res.status(500).json({ message: "Error al obtener información de la API externa." });
+        context.res = {
+            status: 500,
+            body: "Error al obtener información de la API externa."
+        };
     }
-});
-
-module.exports = require("azure-function-express").createAzureFunctionHandler(app);
+};
